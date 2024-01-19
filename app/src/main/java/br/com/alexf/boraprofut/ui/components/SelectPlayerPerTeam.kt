f716d81c11bd9b1b6cf3dcf74c765bccf8f98f19
Alex Felipe <alexfelipevieira@gmail.com>
2024-01-19 19:17:58 -0300

    extracting component select players per team

---
+++ app/src/main/java/br/com/alexf/boraprofut/ui/components/SelectPlayerPerTeam.kt
@@ -0,0 +1,66 @@
+package br.com.alexf.boraprofut.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import br.com.alexf.boraprofut.R
+
+@Composable
+fun SelectPlayerPerTeam(
+    players: Int,
+    onDecreasesPlayers: () -> Unit,
+    onIncreasesPlayers: () -> Unit,
+) {
+    Text(
+        text = stringResource(R.string.players_amount_per_team),
+        style = MaterialTheme.typography.titleLarge
+    )
+    Spacer(modifier = Modifier.size(16.dp))
+    Row(
+        Modifier.height(48.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        val iconModifier = Modifier
+            .fillMaxHeight()
+            .width(40.dp)
+        Icon(
+            Icons.Filled.ArrowBack,
+            contentDescription = stringResource(R.string.decreases_players_amount_per_team),
+            iconModifier
+                .clickable { onDecreasesPlayers() }
+        )
+        Box(Modifier.fillMaxHeight()) {
+            Text(
+                text = "$players",
+                Modifier
+                    .align(Alignment.Center),
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
+        Icon(
+            Icons.Filled.ArrowForward,
+            contentDescription = stringResource(R.string.increases_players_amount_per_team),
+            iconModifier
+                .clickable { onIncreasesPlayers() }
+        )
+
+    }
+}
