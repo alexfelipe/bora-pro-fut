4fb9602b10a39b60ab8c22d1b596667397aa688c
Thailan Godoy <thailanrg@gmail.com>
2024-02-09 09:42:02 -0300

    updating with main branch

--- app/src/main/java/br/com/alexf/boraprofut/MainActivity.kt
+++ app/src/main/java/br/com/alexf/boraprofut/MainActivity.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.rememberNavController
 import br.com.alexf.boraprofut.navigation.BoraProFutNavHost
 import br.com.alexf.boraprofut.ui.theme.BoraProFutTheme
@@ -14,10 +19,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BoraProFutTheme {
-                val navController = rememberNavController()
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    BoraProFutNavHost(navController)
-                }
+
+                var remember by remember { mutableStateOf("") }
+
+                OutlinedTextField(
+                    value = remember,
+                    onValueChange = {
+                        remember = it
+                    }
+                )
             }
         }
     }
