092437a67c16a5c74bb1c6ea8ef6e2b850729a30
Alex Felipe <alexfelipevieira@gmail.com>
2024-02-19 17:23:27 -0300

    fix: avoiding navigation looping when tries to edit players, clear text field and uses the back navigation

--- app/src/main/java/br/com/alexf/boraprofut/navigation/BoraProFutNavHost.kt
+++ app/src/main/java/br/com/alexf/boraprofut/navigation/BoraProFutNavHost.kt
@@ -3,14 +3,16 @@ package br.com.alexf.boraprofut.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.navOptions
 import br.com.alexf.boraprofut.features.balancedTeams.balancedTeams
 import br.com.alexf.boraprofut.features.balancedTeams.navigateToBalancedTeams
 import br.com.alexf.boraprofut.features.drawTeams.drawTeams
 import br.com.alexf.boraprofut.features.drawTeams.drawTeamsRoute
 import br.com.alexf.boraprofut.features.drawTeams.navigateToDrawTeams
 import br.com.alexf.boraprofut.features.game.navigation.gameScreen
-import br.com.alexf.boraprofut.features.playersForm.navigateToPlayersScreen
+import br.com.alexf.boraprofut.features.playersForm.navigateToPlayersFormScreen
 import br.com.alexf.boraprofut.features.playersForm.playersForm
+import br.com.alexf.boraprofut.features.playersForm.playersFormRoute
 import br.com.alexf.boraprofut.features.randomteams.navigateToRandomTeams
 import br.com.alexf.boraprofut.features.randomteams.randomTeams
 
@@ -21,7 +23,11 @@ fun BoraProFutNavHost(navController: NavHostController) {
         startDestination = drawTeamsRoute
     ) {
         playersForm(onNavigateToDrawScreen = {
-            navController.navigateToDrawTeams()
+            navController.navigateToDrawTeams(navOptions {
+                popUpTo(playersFormRoute) {
+                    inclusive = true
+                }
+            })
         })
         drawTeams(
             onNavigateToRandomTeams = {
@@ -30,8 +36,14 @@ fun BoraProFutNavHost(navController: NavHostController) {
             onNavigateToBalancedTeams = {
                 navController.navigateToBalancedTeams()
             },
-            onNavigateToPlayersScreen = {
-                navController.navigateToPlayersScreen()
+            onNavigateToPlayersFormScreen = {
+                navController.navigateToPlayersFormScreen(
+                    navOptions {
+                        popUpTo(navController.graph.id) {
+                            inclusive = true
+                        }
+                    }
+                )
             }
         )
         balancedTeams()
