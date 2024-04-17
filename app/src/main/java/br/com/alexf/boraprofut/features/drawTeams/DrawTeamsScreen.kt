89402b3e0bdcc1571363975fd406a4bea408813d
Janssen Batista <batistajanssen.dev@gmail.com>
2024-04-17 13:27:19 -0300

    feat: add new features to DrawTeamsScreen

--- app/src/main/java/br/com/alexf/boraprofut/features/drawTeams/DrawTeamsScreen.kt
+++ app/src/main/java/br/com/alexf/boraprofut/features/drawTeams/DrawTeamsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -28,7 +29,10 @@ import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.People
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Remove
+import androidx.compose.material.icons.outlined.SportsHandball
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -42,11 +46,11 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import br.com.alexf.boraprofut.R
@@ -66,6 +70,11 @@ import br.com.alexf.boraprofut.ui.theme.PlayersContainerPrimaryColor
 import br.com.alexf.boraprofut.ui.theme.PlayersContainerSecondaryColor
 import kotlin.random.Random
 
+const val PLAYERS_LIST = "PlayersList"
+const val PLAYERS_LIST_ITEM = "PlayersListItem"
+const val PLAYER_LEVEL_MENU = "PlayerLevelMenu"
+const val EDIT_PLAYERS_BUTTON = "EditPlayersButton"
+
 private class DrawOption(
     val title: String,
     val icon: ImageVector,
@@ -73,7 +82,6 @@ private class DrawOption(
     val action: () -> Unit
 )
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DrawTeamsScreen(
     uiState: DrawTeamsUiState,
@@ -84,6 +92,7 @@ fun DrawTeamsScreen(
 ) {
     val context = LocalContext.current
     val totalPlayers = uiState.players.size
+
     Column(
         modifier
             .fillMaxSize()
@@ -104,8 +113,6 @@ fun DrawTeamsScreen(
                 .align(Alignment.CenterHorizontally)
         )
         val options = remember {
-
-
             listOf(
                 DrawOption(
                     title = context.getString(R.string.random),
@@ -161,7 +168,7 @@ fun DrawTeamsScreen(
                     )
                     Spacer(modifier = Modifier.size(16.dp))
                     Icon(
-                        option.icon, contentDescription = null,
+                        option.icon, contentDescription = option.title,
                         Modifier.size(64.dp),
                         tint = Color.White
                     )
@@ -207,158 +214,229 @@ fun DrawTeamsScreen(
             )
         }
         if (uiState.isShowPlayers) {
-            Column {
-                Row(
-                    Modifier
-                        .fillMaxWidth()
-                        .background(
-                            Brush.linearGradient(
-                                listOf(
-                                    PlayersContainerPrimaryColor,
-                                    PlayersContainerSecondaryColor,
-                                    MaterialTheme.colorScheme.background
-                                ),
-                            )
-                        ),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = stringResource(R.string.players),
-                        Modifier.padding(16.dp),
-                        style = MaterialTheme.typography.titleLarge.copy(Color.White)
-                    )
-
+            if (uiState.players.isNotEmpty()) {
+                Column {
                     Row(
                         Modifier
-                            .padding(16.dp)
-                            .clickable {
-                                onEditPlayersClick()
-                            }
-                            .clip(RoundedCornerShape(15))
+                            .fillMaxWidth()
                             .background(
                                 Brush.linearGradient(
                                     listOf(
-                                        EditPlayersButtonContainerPrimaryColor,
-                                        EditPlayersButtonContainerSecondaryColor,
-                                    )
+                                        PlayersContainerPrimaryColor,
+                                        PlayersContainerSecondaryColor,
+                                        MaterialTheme.colorScheme.background
+                                    ),
                                 )
-                            )
-                            .padding(8.dp),
-                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Icon(
-                            imageVector = Icons.Filled.Edit,
-                            contentDescription = stringResource(R.string.players_edit_icon),
-                            tint = Color.White
-                        )
                         Text(
-                            text = stringResource(R.string.edit),
-                            style = LocalTextStyle.current.copy(color = Color.White)
+                            text = stringResource(R.string.players, uiState.players.size),
+                            Modifier.padding(16.dp),
+                            style = MaterialTheme.typography.titleLarge.copy(Color.White)
                         )
-                    }
-                }
-                Column(
-                    Modifier.padding(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(16.dp)
-                ) {
-                    uiState.players.forEach { player ->
+
                         Row(
                             Modifier
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically,
+                                .padding(16.dp)
+                                .clickable {
+                                    onEditPlayersClick()
+                                }
+                                .clip(RoundedCornerShape(15))
+                                .background(
+                                    Brush.linearGradient(
+                                        listOf(
+                                            EditPlayersButtonContainerPrimaryColor,
+                                            EditPlayersButtonContainerSecondaryColor,
+                                        )
+                                    )
+                                )
+                                .padding(8.dp)
+                                .testTag(EDIT_PLAYERS_BUTTON),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
                         ) {
-                            Text(
-                                text = player.name, Modifier.weight(1f),
-                                style = LocalTextStyle.current.copy(
-                                    fontSize = 20.sp,
-                                    fontWeight = FontWeight.Bold
-                                )
+                            Icon(
+                                imageVector = Icons.Filled.Edit,
+                                contentDescription = stringResource(R.string.players_edit_icon),
+                                tint = Color.White
                             )
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                                horizontalArrangement = Arrangement.spacedBy(8.dp)
-                            ) {
-
-                                Box(
-                                    modifier = Modifier
-                                        .clip(CircleShape)
-                                        .combinedClickable(
-                                            onClick = {
-                                                uiState.onDecreasePlayerLevel(player)
-                                            },
-                                            onLongClick = {
-                                                uiState.onDecreasePlayerLevel(player)
-                                            }
-                                        )
-                                        .background(
-                                            DecreasePlayerLevelContainerColor
-                                                .copy(alpha = 0.8f)
-                                        )
-                                        .padding(4.dp)
-                                ) {
-                                    Icon(
-                                        Icons.Outlined.Remove,
-                                        contentDescription = null,
-                                        tint = Color.White
-                                    )
-                                }
-                                Text(
-                                    text = "${player.level}",
-                                    Modifier.width(30.dp),
-                                    style = LocalTextStyle.current.copy(
-                                        fontSize = 20.sp,
-                                        fontWeight = FontWeight.Bold,
-                                        textAlign = TextAlign.Center
-                                    )
-                                )
-
-                                Box(
-                                    modifier = Modifier
-                                        .clip(CircleShape)
-                                        .combinedClickable(
-                                            onClick = {
-                                                uiState.onIncreasePlayerLevel(player)
-                                            },
-                                            onLongClick = {
-
-                                                uiState.onIncreasePlayerLevel(player)
-                                            }
-                                        )
-                                        .background(
-                                            IncreasePlayerLevelContainerColor
-                                                .copy(alpha = 0.8f)
-                                        )
-                                        .padding(4.dp)
-                                ) {
-                                    Icon(
-                                        Icons.Outlined.Add,
-                                        contentDescription = null,
-                                        tint = Color.White
-                                    )
-                                }
-                            }
+                            Text(
+                                text = stringResource(R.string.edit),
+                                style = LocalTextStyle.current.copy(color = Color.White)
+                            )
                         }
                     }
-
+                    PlayersList(uiState = uiState)
                 }
             }
         }
     }
 }
 
+@Composable
+fun PlayersList(uiState: DrawTeamsUiState) {
+    Column(Modifier.testTag(PLAYERS_LIST)) {
+        uiState.players.forEachIndexed { index, player ->
+            PlayersListItem(uiState = uiState, player = player)
+            if (index < uiState.players.size - 1) {
+                HorizontalDivider()
+            }
+        }
+    }
+}
+
+@Composable
+fun PlayersListItem(uiState: DrawTeamsUiState, player: Player) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .testTag(PLAYERS_LIST_ITEM),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(onClick = {
+            uiState.onGoalKeeperChange(player)
+        }, modifier = Modifier.padding(vertical = 16.dp)) {
+            if (player.isGoalKeeper == true) {
+                Icon(
+                    imageVector = Icons.Outlined.SportsHandball,
+                    contentDescription = stringResource(R.string.goal_keeper_icon)
+                )
+            } else {
+                Icon(
+                    imageVector = Icons.Outlined.SportsHandball,
+                    contentDescription = stringResource(R.string.not_goal_keeper_icon),
+                    tint = if (isSystemInDarkTheme()) {
+                        Color.White.copy(alpha = 0.2f)
+                    } else {
+                        Color.Black.copy(alpha = 0.2f)
+                    },
+                )
+            }
+
+        }
+        Text(
+            text = if (player.isGoalKeeper == true) {
+                "${player.name.trim()} (G)"
+            } else {
+                player.name
+            },
+            Modifier.weight(1f),
+            style = LocalTextStyle.current.copy(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+        if (player.isGoalKeeper == false) {
+            PlayerLevelMenu(uiState = uiState, player = player)
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun PlayerLevelMenu(uiState: DrawTeamsUiState, player: Player) {
+    Row(
+        Modifier
+            .padding(end = 16.dp)
+            .testTag(PLAYER_LEVEL_MENU),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .combinedClickable(
+                    onClick = {
+                        uiState.onDecreasePlayerLevel(player)
+                    },
+                    onLongClick = {
+                        uiState.onDecreasePlayerLevel(player)
+                    }
+                )
+                .background(
+                    DecreasePlayerLevelContainerColor
+                        .copy(alpha = 0.8f)
+                )
+                .padding(4.dp)
+        ) {
+            Icon(
+                Icons.Outlined.Remove,
+                contentDescription = stringResource(id = R.string.decrease_level),
+                tint = Color.White
+            )
+        }
+        Text(
+            text = "${player.level}",
+            Modifier.width(28.dp),
+            style = LocalTextStyle.current.copy(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+        )
+
+        Box(
+            modifier = Modifier
+                .clip(CircleShape)
+                .combinedClickable(
+                    onClick = {
+                        uiState.onIncreasePlayerLevel(player)
+                    },
+                    onLongClick = {
+
+                        uiState.onIncreasePlayerLevel(player)
+                    }
+                )
+                .background(
+                    IncreasePlayerLevelContainerColor
+                        .copy(alpha = 0.8f)
+                )
+                .padding(4.dp)
+        ) {
+            Icon(
+                Icons.Outlined.Add,
+                contentDescription = stringResource(id = R.string.increase_level),
+                tint = Color.White
+            )
+        }
+    }
+}
 
 @UiModePreviews
 @Composable
 fun DrawTeamsScreenPreview() {
     BoraProFutTheme {
         DrawTeamsScreen(
-            uiState = DrawTeamsUiState(),
+            uiState = DrawTeamsUiState(isShowPlayers = false),
             onDrawRandomTeamsClick = {},
             onDrawBalancedTeamsClick = {},
             onEditPlayersClick = {}
         )
+    }
+}
+
+@UiModePreviews
+@Composable
+fun PlayersListPreview() {
+
+    val players = mutableSetOf(
+        Player(name = "Alex", level = Random.nextInt(1, 10), isGoalKeeper = true),
+        Player(name = "Thailan", level = Random.nextInt(1, 10)),
+        Player(name = "Daniel", level = Random.nextInt(1, 10)),
+        Player(name = "Joao", level = 10),
+        Player(name = "Janssen", level = Random.nextInt(1, 10))
+    )
+
+    BoraProFutTheme {
+        Surface {
+            PlayersList(
+                uiState = DrawTeamsUiState(
+                    players = players
+                )
+            )
+        }
     }
 }
 
@@ -370,7 +448,11 @@ fun DrawTeamsScreenDisplayingPlayersPreview() {
             DrawTeamsScreen(
                 uiState = DrawTeamsUiState(
                     players = setOf(
-                        Player(name = "Alex", level = Random.nextInt(1, 10)),
+                        Player(
+                            name = "Alex",
+                            level = Random.nextInt(1, 10),
+                            isGoalKeeper = true
+                        ),
                         Player(name = "Thailan", level = Random.nextInt(1, 10)),
                         Player(name = "Daniel", level = Random.nextInt(1, 10)),
                         Player(name = "Joao", level = 10),
